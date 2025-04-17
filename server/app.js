f87e065e87a8d9fb150ae20eebb807ebf8f94213
Saeed Halabi <saeedhalabi80@gmail.com>
2025-04-17 12:12:40 +0300

    Update cors host.

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ const server = http.createServer(app);
 // Create socket server with CORS support
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://whisperly-frontend.onrender.com",
     credentials: true,
   },
 });
--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -1,7 +1,7 @@
 import cors from "cors";
 
 const corsMiddleware = cors({
-  origin: "http://localhost:5173",
+  origin: "https://whisperly-frontend.onrender.com",
   credentials: true,
 });
 
