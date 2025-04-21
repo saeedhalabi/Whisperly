809fb34c7cd18697088ebf759c16ce17300f343f
Saeed Halabi <saeedhalabi80@gmail.com>
2025-04-21 16:11:38 +0300

    remove feature auth provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,15 +4,12 @@ import { BrowserRouter } from "react-router";
 import ChatProvider from "./context/ChatProvider.tsx";
 import "./index.css";
 import App from "./App.tsx";
-import AuthProvider from "./context/AuthProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <ChatProvider>
-        <AuthProvider>
           <App />
-        </AuthProvider>
       </ChatProvider>
     </BrowserRouter>
   </StrictMode>
