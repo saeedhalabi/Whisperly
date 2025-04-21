de7974c2df4a01dbf7407630e1f5725ce08b21c1
Saeed Halabi <saeedhalabi80@gmail.com>
2025-04-21 16:10:34 +0300

    bug fix

--- client/src/context/AuthContext.tsx
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-import { isAuth } from "../types/auth.types";
-
-const AuthContext = createContext<isAuth | null>(null);
-
-export default AuthContext;
--- client/src/router/AppRouter.tsx
+++ client/src/router/AppRouter.tsx
@@ -1,16 +1,24 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 import SignUpPage from "../pages/SignUpPage";
 import SignInPage from "../pages/SignInPage";
 import Homepage from "../pages/Homepage";
 import Unauthorized from "../pages/Unauthorized";
 
 const AppRouter: React.FC = () => {
+  // Check if token is in localStorage
+  const token = localStorage.getItem("token");
+
   return (
     <Routes>
+      {/* Public Routes */}
       <Route index path="/" element={<SignUpPage />} />
       <Route path="/sign-in" element={<SignInPage />} />
 
-      <Route path="/home-page" element={<Homepage />} />
+      {/* Protected Route - Only render Homepage if there's a token */}
+      <Route
+        path="/home-page"
+        element={token ? <Homepage /> : <Navigate to="/sign-in" replace />}
+      />
 
       <Route path="/unauthorized" element={<Unauthorized />} />
     </Routes>
